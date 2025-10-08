bf298d4d583458a3b99e5a4097bce44d08dddc9c
Shariful-Islam-CoU <156356675+Shariful-Islam-CoU@users.noreply.github.com>
2025-10-08 15:36:17 +0600

    Fourth commit

--- server/server.ts
+++ server/server.ts
@@ -11,7 +11,7 @@ const app = (express as any)();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:6300", "http://192.168.88.30:6300"],
+    origin: ["http://localhost:6300", "http://192.168.88.30:6300","https://connect4-vtzu.onrender.com"],
     methods: ["GET", "POST"],
     credentials: true
   },
@@ -20,7 +20,7 @@ const io = new Server(server, {
 
 
 app.use((cors as any)({
-  origin: ["http://localhost:6300", "http://192.168.88.30:6300"],
+  origin: ["http://localhost:6300", "http://192.168.88.30:6300","https://connect4-vtzu.onrender.com"],
   methods: ["GET", "POST"],
   credentials: true
 }));
