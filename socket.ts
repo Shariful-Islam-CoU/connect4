6988c2924d41e1c5fa68e68a1c0808373a2783df
Shariful-Islam-CoU <156356675+Shariful-Islam-CoU@users.noreply.github.com>
2025-10-08 16:38:29 +0600

    Sixth commit

--- socket.ts
+++ socket.ts
@@ -1,7 +1,8 @@
 import {io} from 'socket.io-client';
 
-const socket = io('http://192.168.88.30:6400',{
+// const socket = io('http://192.168.88.30:6400',{
 // const socket = io('https://connect4-server.onrender.com',{
+const socket = io('https://connect4-vtzu.onrender.com',{
     autoConnect:true,
     transports: ['websocket'],
     reconnectionAttempts: 5,
